0f9608fb52edbd1e2a36cbfc984afaf86b8f188f
nevermore111 <2363381916@qq.com>
2023-05-15 13:57:33 +0800

    一些修改，目前还是有bug

--- New Unity Project/Assets/scripts/Character/ArrayPlayableAsset.cs
+++ New Unity Project/Assets/scripts/Character/ArrayPlayableAsset.cs
@@ -5,8 +5,8 @@ using UnityEngine.Timeline;
 [TrackClipType(typeof(ArrayPlayableAsset))]
 public class ArrayPlayableAsset : PlayableAsset, ITimelineClipAsset
 {
+    // 在此处定义其他属性和方法...
     public int[] arrayData;
-
     public ClipCaps clipCaps => ClipCaps.None;
 
     public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
@@ -14,6 +14,16 @@ public class ArrayPlayableAsset : PlayableAsset, ITimelineClipAsset
         var playable = ScriptPlayable<ArrayPlayableBehaviour>.Create(graph);
         var playableBehaviour = playable.GetBehaviour();
         playableBehaviour.arrayData = arrayData;
+        // 从轨道绑定中获取拖放的 GameObject
+        var trackBinding = owner.GetComponent<PlayableDirector>().GetGenericBinding(this) as GameObject;
+
+        if (trackBinding != null)
+        {
+            // 在此处处理拖放的 GameObject
+            Debug.Log("Accessed GameObject: " + trackBinding.name);
+            playableBehaviour.targetObject = trackBinding;
+        }
+
         return playable;
     }
 }
--- New Unity Project/Assets/scripts/Character/ArrayPlayableBehaviour.cs
+++ New Unity Project/Assets/scripts/Character/ArrayPlayableBehaviour.cs
@@ -4,6 +4,7 @@ using UnityEngine.Playables;
 [System.Serializable]
 public class ArrayPlayableBehaviour : PlayableBehaviour
 {
+    public GameObject targetObject;
     public int[] arrayData;
 
     public override void OnBehaviourPlay(Playable playable, FrameData info)
@@ -13,6 +14,11 @@ public class ArrayPlayableBehaviour : PlayableBehaviour
         foreach (int value in arrayData)
         {
             Debug.Log("Array value: " + value);
+        }
+        if (targetObject != null)
+        {
+            // 在此处处理目标对象
+            Debug.Log("Target object name: " + targetObject.name);
         }
     }
 }
