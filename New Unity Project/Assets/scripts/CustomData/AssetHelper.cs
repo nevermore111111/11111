7cec4754426a6bd6ef4e71f0c04b0368409b3383
nevermore111 <2363381916@qq.com>
2023-08-20 17:57:25 +0800

    no message

---
+++ New Unity Project/Assets/scripts/CustomData/AssetHelper.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+[CreateAssetMenu(fileName = "AssetHelper", menuName = "CustomData/AssetHelper")]
+public class AssetHelper : ScriptableObject
+{
+    public PlayableAsset[] AttackOnGround;
+    public PlayableAsset[] AttackOnGround_fist;
+    public PlayableAsset[] AttackInAir;
+    public PlayableAsset[] StartPlay;
+}
+
