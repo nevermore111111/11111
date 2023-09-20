7f6110f8d666f5c1269183b01ffebdaec17c35eb
nevermore111 <2363381916@qq.com>
2023-09-20 08:25:09 +0800

    no message

--- New Unity Project/Assets/Character Controller Pro/Demo/Scripts/character/Weapon/WeaponData.cs
+++ New Unity Project/Assets/Character Controller Pro/Demo/Scripts/character/Weapon/WeaponData.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class WeaponData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float impulse
     {
-        
+        get { return transform.localScale.z; }
+        set
+        {
+            // 设置transform.localScale.x的新值
+            Vector3 newScale = transform.localScale;
+            newScale.z = value;
+            transform.localScale = newScale;
+        }
+    }
+    public float duration
+    {
+        get { return transform.localScale.y; }
+        set
+        {
+            Vector3 newScale = transform.localScale;
+            newScale.y = value;
+            transform.localScale = newScale;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
