34488c18e285bd3e2a9fb95d8ce4b315541cb101
nevermore111 <2363381916@qq.com>
2023-12-21 19:21:17 +0800

    no message

--- New Unity Project/Assets/Character Controller Pro/Demo/Scripts/Camera/CameraShakeTest.cs
+++ New Unity Project/Assets/Character Controller Pro/Demo/Scripts/Camera/CameraShakeTest.cs
@@ -1,10 +1,8 @@
-using DG.Tweening.Core.Enums;
 using DG.Tweening;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using DG.Tweening.Core;
+using DG.Tweening.Core.Enums;
 
 public class CameraShakeTest : MonoBehaviour
 {
@@ -17,19 +15,6 @@ public class CameraShakeTest : MonoBehaviour
         TryGetComponent(out brain);
     }
 
-    public void ShakeCamera2(float strength, float stime, int vibrate)
-    {
-        strength = strength * getCurCameraDistance();
-        shake = Vector3.zero;
-        DOTween.Shake(() => shake, delegate (Vector3 x)
-        {
-            shake = x;
-        }, stime, strength, vibrate, 90f, ignoreZAxis: false, true, 0).SetTarget(shake).SetSpecialStartupMode(SpecialStartupMode.SetShake)
-            .SetOptions(false).OnComplete(() =>
-            {
-                // shake = Vector3.zero;
-            });
-    }
     private float getCurCameraDistance()
     {
         if (brain != null && brain.ActiveVirtualCamera != null)
@@ -39,5 +24,37 @@ public class CameraShakeTest : MonoBehaviour
         }
         Debug.LogError("!错误");
         return -1f;
+    }
+
+    public void Shake(Transform targetTransform, float strength, float stime, int vibrate)
+    {
+        strength = strength * getCurCameraDistance();
+        shake = Vector3.zero;
+        DOTween.Shake(() => shake, v => shake = v, stime, strength, vibrate, 90f, ignoreZAxis: false, true, 0)
+             .SetTarget(shake)
+             .SetSpecialStartupMode(SpecialStartupMode.SetShake)
+             .SetOptions(false)
+             .OnUpdate(() =>
+             {
+                 // 在每一帧更新传入的Transform的位置
+                 targetTransform.position += shake;
+             })
+             .OnComplete(() =>
+             {
+                 shake = Vector3.zero;
+             });
+    }
+
+    public void ShakeCamera2(float strength, float stime, int vibrate)
+    {
+        strength = strength * getCurCameraDistance();
+
+        DOTween.Shake(() => Vector3.zero, v =>
+        {
+            // 在每一帧更新摄像机位置
+            transform.position += v;
+        }, stime, strength, vibrate, 90f, ignoreZAxis: false, true, 0)
+        .SetSpecialStartupMode(SpecialStartupMode.SetShake)
+        .SetOptions(false);
     }
 }
