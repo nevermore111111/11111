116a155efdfee585fd9ec06927d5ff395dbb19be
nevermore111 <2363381916@qq.com>
2023-12-28 19:06:11 +0800

    一些修改

--- New Unity Project/Assets/Character Controller Pro/Demo/Scripts/character/AttackReceive.cs
+++ New Unity Project/Assets/Character Controller Pro/Demo/Scripts/character/AttackReceive.cs
@@ -7,17 +7,20 @@ public class AttackReceive : MonoBehaviour
     public CharacterInfo CharacterInfo;
     public ReceiveHitType receive = ReceiveHitType.Normal;
 
+
+    public ExtremeEvadeArea evadeArea;
+
     public void Awake()
     {
         CharacterInfo = GetComponentInParent<CharacterInfo>();
+        registerHit(receive);
     }
     // 定义一个委托类型，用于表示处理击中的方法
     public delegate void HitHandler();
 
     // 定义事件，根据不同的击中类型注册不同的方法
-    public event HitHandler OnNormalHit;
-    public event HitHandler OnCriticalHit;
-    public event HitHandler OnExtremeEvadeHit;
+    public event HitHandler OnHit;
+
 
     // 定义枚举表示不同的击中类型
     public enum ReceiveHitType
@@ -27,24 +30,37 @@ public class AttackReceive : MonoBehaviour
         ExtremeEvade
     }
 
+    public void onNormalHit()
+    {
+
+    }
+    public void onCriticalHit()
+    {
+        
+    }
+    public void onExtremeEvadeHit()
+    {
+
+    }
+
     // 接收击中的方法，根据击中类型调用相应的事件
-    public void ReceiveHit(ReceiveHitType hitType)
+    public void registerHit(ReceiveHitType hitType)
     {
         switch (hitType)
         {
             case ReceiveHitType.Normal:
                 // 触发普通击中事件
-                OnNormalHit?.Invoke();
+                OnHit += onNormalHit;
                 break;
 
             case ReceiveHitType.Critical:
                 // 触发暴击事件
-                OnCriticalHit?.Invoke();
+                OnHit += onCriticalHit;
                 break;
 
             case ReceiveHitType.ExtremeEvade:
                 // 触发极限闪避事件
-                OnExtremeEvadeHit?.Invoke();
+                OnHit += onExtremeEvadeHit;
                 break;
 
             // 添加其他击中类型的处理
